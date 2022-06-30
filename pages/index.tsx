8980dadfccfef879204e57660821781bf0b0fffb
LawryFa <92519943+LawryFa@users.noreply.github.com>
2022-06-30 04:35:34 +0200

    offers5

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
       </Head>
       <HomepageWrapper>
         <WhiteBackgroundContainer>
-
+<Partners/>
           <BasicSection imageUrl="demo-illustration-1.svg" title="Lorem ipsum dolor sit amet consectetur." overTitle="sit amet gogo">
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore
